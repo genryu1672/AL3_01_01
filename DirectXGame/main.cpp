7468d06c03f9f776975526d6212f71023419cdc4
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2024-04-12 13:04:07 +0900

    ブランチの追加

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -18,6 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	PrimitiveDrawer* primitiveDrawer = nullptr;
 	GameScene* gameScene = nullptr;
 
+
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow();
