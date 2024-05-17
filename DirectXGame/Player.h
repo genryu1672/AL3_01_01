c09d475dd6d9947753c0684a75416a0d405b5e62
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2024-05-17 11:30:38 +0900

    AL3＿０２＿０３

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <Model.h>
+#include <WorldTransform.h>
 class Player {
+public:
+	// 初期化(関数の宣言）
+	void Initialize(Model* model, uint32_t textureHandle);
 
+	// 更新
+	void Update();
+
+	// 描画
+	void Draw(ViewProjection& viewProjection);
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// ViewProjection* viewProjection_ = nullptr;
+	// テクチャハンドル
+	uint32_t textureHandle_ = 0u;
 };
