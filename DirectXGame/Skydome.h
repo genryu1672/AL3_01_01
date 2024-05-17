0bad66378c845712cee93143f7f34f00fbfe3216
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2024-05-17 11:06:08 +0900

    AL3_02_03天球

---
+++ DirectXGame/Skydome.h
@@ -0,0 +1,21 @@
+#pragma once
+class Skydome {
+public:
+	///<summary>
+	///初期化
+	/// </summary>
+	void Initialize();
+
+	///< summary>
+	/// 更新
+	///  </summary>
+	void Update();
+
+	///< summary>
+	/// 描画
+	///  </summary>
+	void Draw();
+
+
+
+};
